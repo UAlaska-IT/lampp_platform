e4488e8dc97b04d3e2b522e62eb3eebe2289c644
Caleb Severn <calsev@gmail.com>
2019-10-26 13:10:45 -0800

    Add database recipe

---
+++ recipes/_database.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+tcb = 'lampp_platform'
+
+mariadb_server_install 'Server' do
+  password(lazy { vault_secret_hash(node[tcb]['database']['root_pw']) }) if node[tcb]['database']['set_root_pw']
+end
+
+mariadb_server_configuration 'Configuration'
+
+mariadb_client_install 'Client'
+
+db_name = node[tcb]['database']['db_name']
+
+mariadb_database db_name
+
+user = node[tcb]['database']['user_name']
+user_pw = vault_secret_hash(node[tcb]['database']['user_pw'])
+
+mariadb_user 'Wiki User' do
+  username user
+  password user_pw
+end
+
+mariadb_user 'Wiki Permissions' do
+  username user
+  password user_pw
+  table '*'
+  database_name db_name
+  privileges [:all]
+  action :grant
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,9 @@
 # frozen_string_literal: true
+
+tcb = 'lampp_platform'
+
+include_recipe 'http_platform::default'
+
+include_recipe "#{tcb}::_install"
+
+include_recipe "#{tcb}::_database"
