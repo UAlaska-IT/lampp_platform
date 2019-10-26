c23a157e9a231cabcdc53d2f47e325285ec57100
Caleb Severn <calsev@gmail.com>
2019-10-26 12:42:17 -0800

    Uplift test helpers

---
+++ test/integration/helpers.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+def apache_user(node)
+  return 'www-data' if node['platform_family'] == 'debian'
+
+  return 'apache'
+end
+
+def apache_group(node)
+  return 'www-data' if node['platform_family'] == 'debian'
+
+  return 'apache'
+end
+
+def apache_service(node)
+  return 'apache2' if node['platform_family'] == 'debian'
+
+  return 'httpd'
+end
+
+def php_prefix(node)
+  return 'php' if node['platform_family'] == 'debian'
+
+  return 'php72'
+end
