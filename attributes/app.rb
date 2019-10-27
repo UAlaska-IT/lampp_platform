132114f9de3e306a3eaca5bccb978cc7d0941425
Caleb Severn <calsev@gmail.com>
2019-10-27 08:29:14 -0800

    Add app attributes

---
+++ attributes/app.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+tcb = 'lampp_platform'
+
+default[tcb]['app']['archive']['download_base_url'] = nil
+default[tcb]['app']['archive']['download_file_name'] = nil
+default[tcb]['app']['archive']['extract_root_directory'] = nil
+default[tcb]['app']['archive']['extract_creates_file'] = nil
+
+default[tcb]['app_updated'] = false
