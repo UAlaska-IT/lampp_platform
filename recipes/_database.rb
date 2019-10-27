904397d03d5c09b44e9a9181c7b78a11fd89cda4
Caleb Severn <calsev@gmail.com>
2019-10-27 11:43:43 -0800

    Gate all mariadb logic

--- recipes/_database.rb
+++ recipes/_database.rb
@@ -4,15 +4,22 @@ tcb = 'lampp_platform'
 
 mariadb_server_install 'Server' do
   password(lazy { vault_default_secret(node[tcb]['database']['root_pw']) }) if node[tcb]['database']['set_root_pw']
+  only_if { default[tcb]['database']['configure_mariadb'] }
 end
 
-mariadb_server_configuration 'Configuration'
+mariadb_server_configuration 'Configuration' do
+  only_if { default[tcb]['database']['configure_mariadb'] }
+end
 
-mariadb_client_install 'Client'
+mariadb_client_install 'Client' do
+  only_if { default[tcb]['database']['configure_mariadb'] }
+end
 
 db_name = node[tcb]['database']['db_name']
 
-mariadb_database db_name
+mariadb_database db_name do
+  only_if { default[tcb]['database']['configure_mariadb'] }
+end
 
 user = node[tcb]['database']['user_name']
 user_pw = vault_default_secret(node[tcb]['database']['user_pw'])
@@ -20,6 +27,7 @@ user_pw = vault_default_secret(node[tcb]['database']['user_pw'])
 mariadb_user 'DB User' do
   username user
   password user_pw
+  only_if { default[tcb]['database']['configure_mariadb'] }
 end
 
 mariadb_user 'DB Permissions' do
@@ -29,4 +37,5 @@ mariadb_user 'DB Permissions' do
   database_name db_name
   privileges [:all]
   action :grant
+  only_if { default[tcb]['database']['configure_mariadb'] }
 end
