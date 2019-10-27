d163dbc51167628d8eb0620518f80c82425bad5b
Caleb Severn <calsev@gmail.com>
2019-10-27 12:41:59 -0800

    Update name of cron file

--- recipes/_backup.rb
+++ recipes/_backup.rb
@@ -20,19 +20,21 @@ time_file = "'#{File.join(backup_dir, time_file)}'"
 latest_file = "'#{File.join(backup_dir, 'backup_latest.sql')}'"
 
 code = <<~CODE
+
   mysqldump -h #{host}  -u #{user} -p '#{pass}' #{database} -c > #{time_file}
   cp #{time_file} #{latest_file}
 CODE
 
 if node['lampp_platform']['database']['backup_to_s3']
   code += <<~CODE
+  
     # Copy both files to S3
     aws s3 cp #{time_file}
     aws s3 cp #{latest_file}
   CODE
 end
 
-cron_d 'LAMPP Backup' do
+cron_d 'lampp_backup' do
   command code
   shell '/bin/bash'
   weekday node[tcb]['database']['backup']['weekday']
