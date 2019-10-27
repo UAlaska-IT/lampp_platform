234e5ed68008a32fc605e1ee255b7dfa1e1cf17a
Caleb Severn <calsev@gmail.com>
2019-10-27 10:25:26 -0800

    Add missing node context in harness

--- test/fixtures/cookbooks/test_harness/recipes/default.rb
+++ test/fixtures/cookbooks/test_harness/recipes/default.rb
@@ -6,9 +6,9 @@ bash 'Public Hostname' do
   code 'hostnamectl set-hostname `curl -s http://169.254.169.254/latest/meta-data/public-hostname`'
 end
 
-default[tcb]['app']['archive']['download_base_url'] = 'cdn.io.alaska.edu/applications'
-default[tcb]['app']['archive']['download_file_name'] = 'phpldapadmin-1.2.3.tgz'
-default[tcb]['app']['archive']['extract_root_directory'] = 'phpldapadmin-1.2.3'
-default[tcb]['app']['archive']['extract_creates_file'] = 'index.php'
+node.default[tcb]['app']['archive']['download_base_url'] = 'cdn.io.alaska.edu/applications'
+node.default[tcb]['app']['archive']['download_file_name'] = 'phpldapadmin-1.2.3.tgz'
+node.default[tcb]['app']['archive']['extract_root_directory'] = 'phpldapadmin-1.2.3'
+node.default[tcb]['app']['archive']['extract_creates_file'] = 'index.php'
 
 include_recipe 'lampp_platform::default'
