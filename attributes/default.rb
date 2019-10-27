f86934446c1e3e91085d88f24abacde5adcc9477
Caleb Severn <calsev@gmail.com>
2019-10-26 18:42:49 -0800

    Add attribute for app name

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
+tcb = 'lampp_platform'
+
 # mod_php does not support threading
 default['http_platform']['apache']['mpm_module'] = 'prefork'
+
+default[tcb]['app_name'] = nil
