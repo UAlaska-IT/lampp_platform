ea066f15fa22c0d1ca495f663359fd42bfb7648c
Caleb Severn <calsev@gmail.com>
2019-10-27 10:11:25 -0800

    Set attributes in test harness

---
+++ test/fixtures/cookbooks/test_harness/attributes/default.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+tcb = 'lampp_platform'
+
+default[tcb]['base_name'] = 'phpldapadmin'
+
+default[tcb]['app']['serve_path'] = 'ldap'
+
+default[tcb]['database']['db_name'] = 'not_used'
+default[tcb]['database']['user_name'] = 'bud'
+
+default[tcb]['database']['root_pw']['vault_bag_item'] = 'lampp'
+default[tcb]['database']['user_pw']['vault_bag_item'] = 'lampp'
--- test/fixtures/cookbooks/test_harness/recipes/default.rb
+++ test/fixtures/cookbooks/test_harness/recipes/default.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
+tcb = 'lampp_platform'
+
 bash 'Public Hostname' do
   code 'hostnamectl set-hostname `curl -s http://169.254.169.254/latest/meta-data/public-hostname`'
 end
+
+default[tcb]['app']['archive']['download_base_url'] = 'cdn.io.alaska.edu/applications'
+default[tcb]['app']['archive']['download_file_name'] = 'phpldapadmin-1.2.3.tgz'
+default[tcb]['app']['archive']['extract_root_directory'] = 'phpldapadmin-1.2.3'
+default[tcb]['app']['archive']['extract_creates_file'] = 'index.php'
 
 include_recipe 'lampp_platform::default'
