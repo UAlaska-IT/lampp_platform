f31492aecbc11a31d3db1b3ee90d6cf313d9af4c
Caleb Severn <calsev@gmail.com>
2019-10-27 16:03:44 -0800

    Simplify privilege test

--- test/integration/default/database_test.rb
+++ test/integration/default/database_test.rb
@@ -16,7 +16,7 @@ installed_command =
 describe bash installed_command do
   its(:exit_status) { should eq 0 }
   # its(:stderr) { should eq '' }
-   # Server
+  # Server
   its(:stdout) { should match(/mariadb-server-10/) }
   its(:stdout) { should match(/postgresql-9/) }
   # Client
@@ -58,19 +58,14 @@ describe bash 'mysql -e \'select User from mysql.user;\'' do
   its(:stdout) { should match(/bud/) }
 end
 
-describe bash 'PGPASSWORD=PasswordIsASecurePassword psql -U bud -h localhost -l' do
-  its(:exit_status) { should eq 0 }
-  its(:stderr) { should eq '' }
-  # TODO: A test for users
-  its(:stdout) { should match(/not_used/) }
-end
+# PostgreSQL user test is implicit in login method
 
 # Access
 
 describe bash 'mysql -e \'show grants for bud@localhost;\'' do
   its(:exit_status) { should eq 0 }
   its(:stderr) { should eq '' }
-  its(:stdout) { should match(/GRANT ALL PRIVILEGES ON `not_used`\.* TO 'bud'@'localhost'/) }
+  its(:stdout) { should match(/GRANT ALL PRIVILEGES/) }
 end
 
 describe bash 'PGPASSWORD=PasswordIsASecurePassword psql -U bud -h localhost -l' do
