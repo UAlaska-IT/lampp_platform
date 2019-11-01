ff4f29e481dc9da564c71232ab3d58dbbc146427
Caleb Severn <calsev@gmail.com>
2019-11-01 12:41:39 -0800

    Setup for database_application

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,10 +4,8 @@ tcb = 'lampp_platform'
 
 include_recipe 'http_platform::default'
 
+include_recipe 'database_application:: default' if node[tcb]['configure_local_database']
+
 include_recipe "#{tcb}::_install"
 
-include_recipe "#{tcb}::_database"
-
 include_recipe "#{tcb}::_app"
-
-include_recipe "#{tcb}::_backup" if node[tcb]['database']['configure_backup']
