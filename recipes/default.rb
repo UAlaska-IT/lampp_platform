21e7054873cf564a4f67ce5e8df02ad220f125f4
Caleb Severn <calsev@gmail.com>
2019-11-11 16:24:50 -0900

    Fix database recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,9 @@ tcb = 'lampp_platform'
 
 include_recipe 'http_platform::default'
 
-include_recipe 'database_application:: default' if node[tcb]['configure_local_database']
+include_recipe 'database_application::server' if node[tcb]['configure_local_database']
+
+include_recipe 'database_application::client'
 
 include_recipe "#{tcb}::_install"
 
--- test/fixtures/cookbooks/test_harness/recipes/default.rb
+++ test/fixtures/cookbooks/test_harness/recipes/default.rb
@@ -6,6 +6,7 @@ bash 'Public Hostname' do
   code 'hostnamectl set-hostname `curl -s http://169.254.169.254/latest/meta-data/public-hostname`'
 end
 
+# Test lazy logic
 node.default[tcb]['app']['archive']['download_base_url'] = 'https://github.com/bcit-ci/CodeIgniter/archive'
 node.default[tcb]['app']['archive']['download_file_link'] = '3.1.11.zip'
 node.default[tcb]['app']['archive']['download_file_name'] = 'CodeIgniter-3.1.11.zip'
