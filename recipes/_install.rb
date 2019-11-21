093f8aaeb0be8c1774878d5411aff47b2971102d
Caleb Severn <calsev@gmail.com>
2019-11-20 17:33:07 -0900

    Copy php conf on centos

--- recipes/_install.rb
+++ recipes/_install.rb
@@ -52,3 +52,28 @@ package "mod_#{php_prefix}" unless is_debian
 apache2_module 'php7' do
   mod_name php_module_name
 end
+
+# The PHP conf is not being enabled on CentOS, then conf.d is deleted by the apache2 cookbook on the next run
+[
+  'php.conf',
+].each do |conf|
+  from_path = "file://#{conf_root_directory}/conf.d/#{conf}"
+  to_path = File.join(conf_available_directory, conf)
+
+  remote_file to_path do
+    source from_path
+    owner 'root'
+    group 'root'
+    mode '644'
+    action :nothing
+    subscribes :create, 'apache2_module[php7]', :immediate
+    notifies :restart, 'service[apache2]', :delayed
+    not_if { platform_family?('debian') }
+  end
+
+  link File.join(conf_enabled_directory, conf) do
+    to to_path
+    notifies :restart, 'service[apache2]', :delayed
+    not_if { platform_family?('debian') }
+  end
+end
